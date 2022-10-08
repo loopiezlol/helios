6da50f4235b921a453f72ff633819cba28ce8f37
Catalin Buruiana <loopiezlol@gmail.com>
2022-10-08 12:05:00 +0100

    :sparkles: hardcode folder url in premiumize

--- projects/plugin/src/plugin/services/premiumize/forms/transfer/premiumize-transfer-create.form.ts
+++ projects/plugin/src/plugin/services/premiumize/forms/transfer/premiumize-transfer-create.form.ts
@@ -4,7 +4,8 @@ import { PremiumizeTransferCreateDto } from '../../dtos/transfer/premiumize-tran
 export class PremiumizeTransferCreateForm {
   static submit(torrentUrl: string) {
     return PremiumizeApiService.post<PremiumizeTransferCreateDto>('/transfer/create', {
-      src: torrentUrl
+      src: torrentUrl,
+      folder_id: 'PeseXLOQskTBI96fIcwiZw'
     });
   }
 }
